15f9766d67d53f298c4a4e221cdaa690c1c544a0
esellers25 <erin.e.sellers@gmail.com>
2021-07-21 14:19:43 -0400

    done

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,10 @@
 function reverseString(str) {
-  // type your code here
+  let reverse = ""; 
+  for (let i = str.length - 1; i > -1; --i){
+    reverse = reverse + str[i]
+  }
+
+  return reverse
 }
 
 if (require.main === module) {
@@ -17,3 +22,9 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// initialize a variable called reverse and set it equal to an empty string
+
+// iterate through the input string; 
+// set reverse equal to reverse + new character 
+// return reverse 
